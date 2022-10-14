7205830ca30fbef6b8251d51c7498589e7d73d98
lucianawaka <lucianawakabayashi@gmail.com>
2022-10-14 19:26:01 +0000

    Previsor page

--- 1 - Mental Helth in Tech Survey/app.py
+++ 1 - Mental Helth in Tech Survey/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pickle
 
 def main():
     options = ['Homepage','Consulta','Previsão']
-    page_option = st.sidebar.selectbox['Options', options]
+    page_option = st.sidebar.selectbox('Options', options)
     
     if page_option == 'Homepage':
         homepage()
@@ -11,5 +12,25 @@ def main():
     elif page_option == 'Previsão':
         previsao()
 
+
+# Home page
+
+def homepage():
+    pass
+
+def consulta():
+    pass
+
+def previsao():
+    if st.button('Submeter'):
+        print('Previsão dados')
+        model = pickle.load(open("model/RandomForestClassifier_v1.pkl"))
+        meus_dados = [[31,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,4,0]]
+        predict_treatment = model.predict(meus_dados)
+        predict_treatment_t = type(predict_treatment)
+        st.markdown(f"""Previsão:{predict_treatment} 
+                    Tipo: {predict_treatment_t}
+                    """)
+        
 if __name__ == '__main__':
     main()
