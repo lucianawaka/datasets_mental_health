61fe4891c5f6d7962fe9791827c5bff844e3df66
lucianawaka <lucianawakabayashi@gmail.com>
2022-10-14 18:55:34 +0000

    docker-compose command run update

--- 1 - Mental Helth in Tech Survey/app.py
+++ 1 - Mental Helth in Tech Survey/app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title("Hello World, is comming back!")
+    options = ['Homepage','Consulta','Previsão']
+    page_option = st.sidebar.selectbox['Options', options]
+    
+    if page_option == 'Homepage':
+        homepage()
+    elif page_option == 'Consulta':
+        consulta()
+    elif page_option == 'Previsão':
+        previsao()
 
 if __name__ == '__main__':
     main()
