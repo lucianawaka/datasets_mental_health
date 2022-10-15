158855b6b0f7ead01528afcd978ed74be386d946
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-10-15 17:44:06 -0300

    Texto de Introdução

--- 1 - Mental Helth in Tech Survey/app.py
+++ 1 - Mental Helth in Tech Survey/app.py
@@ -19,30 +19,25 @@ def main():
 
 def homepage():
     st.markdown(f"""
-                ### Target: treatment: Have you sought treatment for a mental health condition?
-                
-                - Age: Idade.
-                - Gender: Gênero.
-                - self_employed: Are you self-employed?
-                - family_history: Do you have a family history of mental illness?
-                - remote_work: Do you work remotely (outside of an office) at least 50% of the time?
-                - tech_company: Is your employer primarily a tech company/organization?
-                - benefits: Does your employer provide mental health benefits?
-                - care_options: Do you know the options for mental health care your employer provides?
-                - wellness_program: Has your employer ever discussed mental health as part of an employee wellness program?
-                - seek_help: Does your employer provide resources to learn more about mental health issues and how to seek help?
-                - anonymity: Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?
-                - mental health consequence: Do you think that discussing a mental health issue with your employer would have negative consequences?
-                - physhealthconsequence: Do you think that discussing a physical health issue with your employer would have negative consequences?
-                - coworkers: Would you be willing to discuss a mental health issue with your coworkers?
-                - supervisor: Would you be willing to discuss a mental health issue with your direct supervisor(s)?
-                - mental health interview: Would you bring up a mental health issue with a potential employer in an interview?
-                - physhealth interview: Would you bring up a physical health issue with a potential employer in an interview?
-                - mental vs physical: Do you feel that your employer takes mental health as seriously as physical health?
-                - obs_consequence: Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?
-                - work_interfere: If you have a mental health condition, do you feel that it interferes with your work?
-                - no_employees: How many employees does your company or organization have?
-                - leave: How easy is it for you to take medical leave for a mental health condition?             
+                #### Nosso conjunto de dados é composto da saúde mental e frequência de distúrbios de saúde mental no local de trabalho de tecnologia.
+
+                ### Pergunta a responder:
+                A pessoa faria um tratamento para saúde mental?
+                - Quais são as características mais influentes?
+                - Neste conjunto de dados é possível responder se uma pessoa irá procurar de tratamento ou não?
+
+                #### O conjunto de dados:
+                Tamanho: 1259 linhas e 27 recursos
+
+                Sim: 50.595711
+                Não: 49.404289
+
+                #### Conjunto de dados Saúde Mental em TI
+                "Este conjunto de dados é de uma pesquisa de 2014 que mede as atitudes em relação à saúde mental e a frequência de distúrbios de saúde mental no local de trabalho de tecnologia." (De Kanggle)
+
+                Link: [Fonte de dados do Kanggle](https://www.kaggle.com/datasets/osmi/mental-health-in-tech-survey?resource=download&select=survey.csv)
+
+                Versão 1.0 - 15/12/2022          
                 
                 """)
 
