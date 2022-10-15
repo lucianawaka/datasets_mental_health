efe3bb3398fe452de0f020b7fef107833da40b5e
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-10-15 07:34:15 -0300

    Homepage start context

--- 1 - Mental Helth in Tech Survey/app.py
+++ 1 - Mental Helth in Tech Survey/app.py
@@ -18,7 +18,34 @@ def main():
 # Home page
 
 def homepage():
-    pass
+    st.markdown(f"""
+                ### Target: treatment: Have you sought treatment for a mental health condition?
+                <ol>
+                'Age'
+                'Gender'
+                <li>self_employed: Are you self-employed?</li>
+                <li>family_history: Do you have a family history of mental illness?</li>
+                <li>remote_work: Do you work remotely (outside of an office) at least 50% of the time?</li>
+                <li>tech_company: Is your employer primarily a tech company/organization?</li>
+                <li>benefits: Does your employer provide mental health benefits?</li>
+                <li>care_options: Do you know the options for mental health care your employer provides?</li>
+                <li>wellness_program: Has your employer ever discussed mental health as part of an employee wellness program?</li>
+                <li>seek_help: Does your employer provide resources to learn more about mental health issues and how to seek help?</li>
+                <li>anonymity: Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?</li>
+                'mental_health_consequence'
+                'phys_health_consequence'
+                'coworkers'
+                'supervisor'
+                'mental_health_interview'
+                'phys_health_interview'
+                'mental_vs_physical'
+                'obs_consequence'
+                'work_interfere_num'
+                'no_employees_num'
+                <li>leave: How easy is it for you to take medical leave for a mental health condition?</li>
+                </ol>              
+                
+                """)
 
 def consulta():
     pass
