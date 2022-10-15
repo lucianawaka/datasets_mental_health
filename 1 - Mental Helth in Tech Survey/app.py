fb1c0cad432cdb7002ae9b574ecc8e7fa760edc1
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2022-10-15 07:41:00 -0300

    Context in english

--- 1 - Mental Helth in Tech Survey/app.py
+++ 1 - Mental Helth in Tech Survey/app.py
@@ -20,30 +20,29 @@ def main():
 def homepage():
     st.markdown(f"""
                 ### Target: treatment: Have you sought treatment for a mental health condition?
-                <ol>
-                'Age'
-                'Gender'
-                <li>self_employed: Are you self-employed?</li>
-                <li>family_history: Do you have a family history of mental illness?</li>
-                <li>remote_work: Do you work remotely (outside of an office) at least 50% of the time?</li>
-                <li>tech_company: Is your employer primarily a tech company/organization?</li>
-                <li>benefits: Does your employer provide mental health benefits?</li>
-                <li>care_options: Do you know the options for mental health care your employer provides?</li>
-                <li>wellness_program: Has your employer ever discussed mental health as part of an employee wellness program?</li>
-                <li>seek_help: Does your employer provide resources to learn more about mental health issues and how to seek help?</li>
-                <li>anonymity: Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?</li>
-                'mental_health_consequence'
-                'phys_health_consequence'
-                'coworkers'
-                'supervisor'
-                'mental_health_interview'
-                'phys_health_interview'
-                'mental_vs_physical'
-                'obs_consequence'
-                'work_interfere_num'
-                'no_employees_num'
-                <li>leave: How easy is it for you to take medical leave for a mental health condition?</li>
-                </ol>              
+                
+                - Age: Idade.
+                - Gender: Gênero.
+                - self_employed: Are you self-employed?
+                - family_history: Do you have a family history of mental illness?
+                - remote_work: Do you work remotely (outside of an office) at least 50% of the time?
+                - tech_company: Is your employer primarily a tech company/organization?
+                - benefits: Does your employer provide mental health benefits?
+                - care_options: Do you know the options for mental health care your employer provides?
+                - wellness_program: Has your employer ever discussed mental health as part of an employee wellness program?
+                - seek_help: Does your employer provide resources to learn more about mental health issues and how to seek help?
+                - anonymity: Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?
+                - mental health consequence: Do you think that discussing a mental health issue with your employer would have negative consequences?
+                - physhealthconsequence: Do you think that discussing a physical health issue with your employer would have negative consequences?
+                - coworkers: Would you be willing to discuss a mental health issue with your coworkers?
+                - supervisor: Would you be willing to discuss a mental health issue with your direct supervisor(s)?
+                - mental health interview: Would you bring up a mental health issue with a potential employer in an interview?
+                - physhealth interview: Would you bring up a physical health issue with a potential employer in an interview?
+                - mental vs physical: Do you feel that your employer takes mental health as seriously as physical health?
+                - obs_consequence: Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?
+                - work_interfere: If you have a mental health condition, do you feel that it interferes with your work?
+                - no_employees: How many employees does your company or organization have?
+                - leave: How easy is it for you to take medical leave for a mental health condition?             
                 
                 """)
 
